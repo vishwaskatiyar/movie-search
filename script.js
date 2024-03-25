f18b116c312822a26d97c4f025c7322e11744551
Vishwas katiyar <vishwaskatiyar39@gmail.com>
2024-03-26 01:04:21 +0530

    final commit

--- script.js
+++ script.js
@@ -1,64 +1,283 @@
-const searchInput = document.getElementById('search-movie');
-const movieContainer = document.getElementById('movie-container');
-const paginationContainer = document.getElementById('pagination-container');
+//TMDB
 
-const Api_Key = '6cb964bf';
-const Base_Url = `http://www.omdbapi.com/?i=tt3896198&apikey=${Api_Key}`;
+const API_KEY = "api_key=1cf50e6248dc270629e802686245c2c8";
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
+const searchURL = BASE_URL + "/search/movie?" + API_KEY;
 
-const debounce = (fn, delay) => {
-    let timer;
-    return function () {
-        timer && clearTimeout(timer);
-        timer = setTimeout(() => {
-            fn();
-        }, delay);
-    };
-};
+const genres = [
+    {
+        id: 28,
+        name: "Action",
+    },
+    {
+        id: 12,
+        name: "Adventure",
+    },
+    {
+        id: 16,
+        name: "Animation",
+    },
+    {
+        id: 35,
+        name: "Comedy",
+    },
+    {
+        id: 80,
+        name: "Crime",
+    },
+    {
+        id: 99,
+        name: "Documentary",
+    },
+    {
+        id: 18,
+        name: "Drama",
+    },
+    {
+        id: 10751,
+        name: "Family",
+    },
+    {
+        id: 14,
+        name: "Fantasy",
+    },
+    {
+        id: 36,
+        name: "History",
+    },
+    {
+        id: 27,
+        name: "Horror",
+    },
+    {
+        id: 10402,
+        name: "Music",
+    },
+    {
+        id: 9648,
+        name: "Mystery",
+    },
+    {
+        id: 10749,
+        name: "Romance",
+    },
+    {
+        id: 878,
+        name: "Science Fiction",
+    },
+    {
+        id: 10770,
+        name: "TV Movie",
+    },
+    {
+        id: 53,
+        name: "Thriller",
+    },
+    {
+        id: 10752,
+        name: "War",
+    },
+    {
+        id: 37,
+        name: "Western",
+    },
+];
 
-async function fetchMovie(page = 1) {
-    const response = await fetch(`${Base_Url}&s=${searchInput.value}&page=${page}`);
-    const data = await response.json();
-    const { Response, Search, totalResults } = data;
-    Response === "False" ? responseIsFalse() : displayMoviesContainer(Search, totalResults, page);
-    console.log(data);
-}
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const tagsEl = document.getElementById("tags");
 
-const debounceFunction = debounce(fetchMovie, 1000);
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const current = document.getElementById("current");
 
-searchInput.addEventListener('input', debounceFunction);
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = "";
+var totalPages = 100;
 
-function responseIsFalse() {
-    movieContainer.innerHTML = `<h2 class="no-movies">No movies were found for your search input "${searchInput.value}"</h2>`;
-    paginationContainer.innerHTML = ""; // Clear pagination if no results found
-}
-
-function displayMoviesContainer(Search, totalResults, page) {
-    movieContainer.innerHTML = "";
-    Search.forEach((movie) => {
-        const card = createCard(movie);
-        movieContainer.appendChild(card);
+var selectedGenre = [];
+setGenre();
+function setGenre() {
+    tagsEl.innerHTML = "";
+    genres.forEach((genre) => {
+        const t = document.createElement("div");
+        t.classList.add("tag");
+        t.id = genre.id;
+        t.innerText = genre.name;
+        t.addEventListener("click", () => {
+            if (selectedGenre.length == 0) {
+                selectedGenre.push(genre.id);
+            } else {
+                if (selectedGenre.includes(genre.id)) {
+                    selectedGenre.forEach((id, idx) => {
+                        if (id == genre.id) {
+                            selectedGenre.splice(idx, 1);
+                        }
+                    });
+                } else {
+                    selectedGenre.push(genre.id);
+                }
+            }
+            console.log(selectedGenre);
+            getMovies(API_URL + "&with_genres=" + encodeURI(selectedGenre.join(",")));
+            highlightSelection();
+        });
+        tagsEl.append(t);
     });
-    displayPagination(totalResults, page);
 }
 
-const createCard = (movie) => {
-    const card = document.createElement("div");
-    card.className = "card";
-    card.innerHTML = `
-    <img src="${movie.Poster}" class="card-img-top" alt="...">
-    <div class="card-body">
-    <h3>${movie.Title}</h3>
-    <p>${movie.Year}</p>
-    </div>`;
-    return card;
-};
+function highlightSelection() {
+    const tags = document.querySelectorAll(".tag");
+    tags.forEach((tag) => {
+        tag.classList.remove("highlight");
+    });
+    clearBtn();
+    if (selectedGenre.length != 0) {
+        selectedGenre.forEach((id) => {
+            const hightlightedTag = document.getElementById(id);
+            hightlightedTag.classList.add("highlight");
+        });
+    }
+}
 
-const displayPagination = (totalResults, currentpage) => {
-    const totalPages = Math.ceil(totalResults / 10);
-    paginationContainer.innerHTML = `
-    <button ${currentpage === 1 ? "disabled" : ""} onclick="fetchMovie(${currentpage - 1})">Previous</button>
-    <span>${currentpage} of ${totalPages}</span>
-    <button ${currentpage === totalPages ? "disabled" : ""} onclick="fetchMovie(${currentpage + 1})">Next</button>`;
-};
+function clearBtn() {
+    let clearBtn = document.getElementById("clear");
+    if (clearBtn) {
+        clearBtn.classList.add("highlight");
+    } else {
+        let clear = document.createElement("div");
+        clear.classList.add("tag", "highlight");
+        clear.id = "clear";
+        clear.innerText = "Clear x";
+        clear.addEventListener("click", () => {
+            selectedGenre = [];
+            setGenre();
+            getMovies(API_URL);
+        });
+        tagsEl.append(clear);
+    }
+}
 
-let currentpage = 1;
+getMovies(API_URL);
+
+function getMovies(url) {
+    lastUrl = url;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data.results);
+            if (data.results.length !== 0) {
+                showMovies(data.results);
+                currentPage = data.page;
+                nextPage = currentPage + 1;
+                prevPage = currentPage - 1;
+                totalPages = data.total_pages;
+
+                current.innerText = currentPage;
+
+                if (currentPage <= 1) {
+                    prev.classList.add("disabled");
+                    next.classList.remove("disabled");
+                } else if (currentPage >= totalPages) {
+                    prev.classList.remove("disabled");
+                    next.classList.add("disabled");
+                } else {
+                    prev.classList.remove("disabled");
+                    next.classList.remove("disabled");
+                }
+
+                tagsEl.scrollIntoView({ behavior: "smooth" });
+            } else {
+                main.innerHTML = `<h1 class="no-results">No Results Found</h1>`;
+            }
+        });
+}
+
+function showMovies(data) {
+    main.innerHTML = "";
+
+    data.forEach((movie) => {
+        const { title, poster_path, vote_average, overview, id } = movie;
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+        movieEl.innerHTML = `
+             <img src="${poster_path
+                ? IMG_URL + poster_path
+                : "http://via.placeholder.com/1080x1580"
+            }" alt="${title}">
+
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+
+            <div class="overview">
+
+                <h3>Overview</h3>
+                ${overview}
+                <br/> 
+                <button class="know-more" id="${id}">Know More</button
+            </div>
+        
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getColor(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    selectedGenre = [];
+    setGenre();
+    if (searchTerm) {
+        getMovies(searchURL + "&query=" + searchTerm);
+    } else {
+        getMovies(API_URL);
+    }
+});
+
+prev.addEventListener("click", () => {
+    if (prevPage > 0) {
+        pageCall(prevPage);
+    }
+});
+
+next.addEventListener("click", () => {
+    if (nextPage <= totalPages) {
+        pageCall(nextPage);
+    }
+});
+
+function pageCall(page) {
+    let urlSplit = lastUrl.split("?");
+    let queryParams = urlSplit[1].split("&");
+    let key = queryParams[queryParams.length - 1].split("=");
+    if (key[0] != "page") {
+        let url = lastUrl + "&page=" + page;
+        getMovies(url);
+    } else {
+        key[1] = page.toString();
+        let a = key.join("=");
+        queryParams[queryParams.length - 1] = a;
+        let b = queryParams.join("&");
+        let url = urlSplit[0] + "?" + b;
+        getMovies(url);
+    }
+}
